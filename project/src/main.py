7e0fa4cd0d0c82605be415943d90a917a18ee222
luorixo <luorixo@gmail.com>
2024-06-04 21:21:54 +1200

    add alfa poisoner

--- project/src/main.py
+++ project/src/main.py
@@ -1,5 +1,10 @@
 import argparse
-from data_generators.difficulty_generator import DifficultyGenerator
+import glob
+import os
+from pathlib import Path
+
+from datagenerators.difficultygenerator import DifficultyGenerator
+from alfapoison import alfa_poison
 
 def main():
     parser = argparse.ArgumentParser()
@@ -7,13 +12,37 @@ def main():
                         help='# of random generated synthetic data sets.')
     parser.add_argument('-f', '--folder', default='synth', type=str,
                         help='The output folder.')
+    parser.add_argument('-o', '--output', type=str, default='results/synth',
+                        help='The output path for scores.')
+    parser.add_argument('-s', '--step', type=float, default=0.05,
+                        help='Spacing between values for poisoning rates. Default=0.05')
+    parser.add_argument('-m', '--max', type=float, default=0.41,
+                        help='End of interval for poisoning rates. Default=0.41')
     args = parser.parse_args()
-    
-    # Create an instance of DifficultyGenerator
-    generator = DifficultyGenerator(args.nSets, args.folder)
-    
+
     # Generate synthetic datasets
+    generator = DifficultyGenerator(args.nSets, args.folder)
     generator.synth_data_grid()
 
-if __name__ == '__main__':
+    # Perform ALFA poisoning attack
+    filepath = str(Path(args.folder).absolute())
+    output = str(Path(args.output).absolute())
+    step = args.step
+    max = args.max
+
+    advxrange = np.arange(0, max, step)
+
+    print('Path:', filepath)
+    print('Range:', advxrange)
+
+    trainlist = sorted(glob.glob(os.path.join(filepath, 'train', '.csv')))
+    test_list = sorted(glob.glob(os.path.join(filepath, 'test', '.csv')))
+    assert len(trainlist) == len(testlist)
+    print('Found {} datasets'.format(len(train_list)))
+
+    for train, test in zip(train_list, test_list):
+        dataset = {'train': train, 'test': test}
+        alfa_poison(dataset, advx_range, output)
+
+if __name == '__main':
     main()
