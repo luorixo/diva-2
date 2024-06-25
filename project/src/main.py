df8014405f0067cc0083f0bbd6a0aaea611315b6
luorixo <luorixo@gmail.com>
2024-06-26 01:05:44 +1200

    feat: Add script to create metadb

--- project/src/main.py
+++ project/src/main.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 from data_generators.difficulty_generator import DifficultyGenerator
 from poisoners.alfa_poisoner import alfa_poison
+from meta_database.make_metadb import make_metadb
 from utils.test_train_split import test_train_split
 from meta_database.extract_complexity_measures import extract_complexity_measures
 from memento import Config, Context, Memento
@@ -28,7 +29,7 @@ def poison_experiment(context: Context, config: Config):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', '--nSets', default=10, type=int,
+    parser.add_argument('-n', '--nSets', default=8, type=int,
                         help='# of random generated synthetic data sets.')
     parser.add_argument('-f', '--folder', default='', type=str,
                         help='The output folder.')
@@ -79,5 +80,8 @@ def main():
     # Print the complexity measures
     print(complexity_measures_df)
 
+    # Create metadata database
+    csv_path = os.path.join(output, 'synth_alfa_svm_score.csv')
+    make_metadb(csv_path, complexity_measures_df, os.path.join(output, 'metadata.csv'))
 if __name__ == '__main__':
     main()
