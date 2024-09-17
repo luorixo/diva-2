54bfe161c0fc789e0f3aa9b55355c381e35af4a1
Tinchuu <clem.sun20@gmail.com>
2024-09-18 10:31:00 +1200

    less

--- diva/svm_poissvm/svm_poissvm_generate_metadb.py
+++ diva/svm_poissvm/svm_poissvm_generate_metadb.py
@@ -29,7 +29,7 @@ from sklearn.svm import SVC
 np.random.seed(301)
 
 def generate_synthetic_data(n_sets, folder):
-    N_SAMPLES = np.arange(400, 800, 200)
+    N_SAMPLES = np.arange(200, 300, 200)
     N_CLASSES = 2 
 
     data_path = os.path.join('clean_data', folder)
